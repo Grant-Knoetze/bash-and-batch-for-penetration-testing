17def327be8e7927d4d2c0c2d423176188c52e92
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2025-02-24 14:29:26 +0200

    Initial commit for informationGathering.sh

---
+++ informationGathering.sh
@@ -0,0 +1,22 @@
+!#/bin/bash
+
+# This script collects basic information on a system for debugging:
+
+echo "Starting at: $(date)"
+echo 
+
+echo "UPTIME"
+UPTIME
+echo
+
+echo "FREE"
+FREE
+echo
+
+echo "WHO"
+WHO
+echo
+
+echo "Finishing at: $(date)"
+
+
