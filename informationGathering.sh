7c06401122eb65087b5e74cca8929d6c7e0e3985
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2025-02-24 14:31:45 +0200

    Change syntax for improved readability in informationGathering.sh

--- informationGathering.sh
+++ informationGathering.sh
@@ -2,20 +2,13 @@
 
 # This script collects basic information on a system for debugging:
 
-echo "Starting at: $(date)"
-echo 
+echo "Starting at: $(date)"; echo 
 
-echo "UPTIME"
-UPTIME
-echo
+echo "UPTIME"; UPTIME; echo
 
-echo "FREE"
-FREE
-echo
+echo "FREE"; FREE; echo
 
-echo "WHO"
-WHO
-echo
+echo "WHO"; WHO; echo
 
 echo "Finishing at: $(date)"
 
